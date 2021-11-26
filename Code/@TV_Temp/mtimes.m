05e495f9a02cde3d1a2c064d4b234e2615acc324
Zekang Ding <55080829+DingZeKang@users.noreply.github.com>
2021-11-26 13:38:30 +0800

    Add files via upload

---
+++ Code/@TV_Temp/mtimes.m
@@ -0,0 +1,36 @@
+function res = mtimes(a,b)
+
+if a.adjoint
+%     res = adjDx(b(:,:,:,:,1)) + adjDy(b(:,:,:,:,2)) + adjDz(b(:,:,:,:,3)) + adjDt(b(:,:,:,:,4));
+    res = adjDt(b);
+else
+%     Dx = b([2:end,end],:,:,:) - b;
+%     Dy = b(:,[2:end,end],:,:) - b;
+%     Dz = b(:,:,[2:end,end],:) - b;
+%     Dt = b(:,:,:,[2:end,end]) - b;
+%     res = cat(5,Dx,Dy,Dz,Dt); % cat: connect matrix at the 5th dimension
+    res=b(:,:,:,[2:end,end])-b;
+end
+
+% function res = adjDx(x)
+% res = x([1,1:end-1],:,:,:) - x;
+% res(1,:,:,:) = -x(1,:,:,:);
+% res(end,:,:,:) = x(end-1,:,:,:);
+% 
+% function res = adjDy(x)
+% res = x(:,[1,1:end-1],:,:) - x;
+% res(:,1,:,:) = -x(:,1,:,:);
+% res(:,end,:,:) = x(:,end-1,:,:);
+% 
+% function res = adjDz(x)
+% res = x(:,:,[1,1:end-1],:) - x;
+% res(:,:,1,:) = -x(:,:,1,:);
+% res(:,:,end,:) = x(:,:,end-1,:);
+
+function res = adjDt(x)
+res = x(:,:,:,[1,1:end-1]) - x;
+res(:,:,:,1) = -x(:,:,:,1);
+res(:,:,:,end) = x(:,:,:,end-1);
+
+
+
---
+++ Code/@TV_Temp_3D/mtimes.m
@@ -0,0 +1,25 @@
+function res = mtimes(a,b)
+
+if a.adjoint
+    res = adjDx(b(:,:,:,:,1)) + adjDy(b(:,:,:,:,2)) + adjDz(b(:,:,:,:,3));
+else
+    Dx = b([2:end,end],:,:,:) - b;
+    Dy = b(:,[2:end,end],:,:) - b;
+    Dz = b(:,:,[2:end,end],:) - b;
+    res = cat(5,Dx,Dy,Dz); % cat: connect matrix at the 4th dimension
+end
+
+function res = adjDx(x)
+res = x([1,1:end-1],:,:,:) - x;
+res(1,:,:,:) = -x(1,:,:,:);
+res(end,:,:,:) = x(end-1,:,:,:);
+
+function res = adjDy(x)
+res = x(:,[1,1:end-1],:,:) - x;
+res(:,1,:,:) = -x(:,1,:,:);
+res(:,end,:,:) = x(:,end-1,:,:);
+
+function res = adjDz(x)
+res = x(:,:,[1,1:end-1],:) - x;
+res(:,:,1,:) = -x(:,:,1,:);
+res(:,:,end,:) = x(:,:,end-1,:);
